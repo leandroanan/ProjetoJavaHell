e684e641bb0f26850146ae974f475dead99888e3
leandroanan <leandro_rodrigues2008@hotmail.com>
2016-04-30 15:52:07 -0300

    Teste commit 2

---
+++ ProjetoJavaHell/ProjetoJavaHell/src/main/java/Model/Conexao.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author AX4B
+ */
+public class Conexao {
+    
+    public static Connection obterConexao() throws SQLException, ClassNotFoundException {
+        Connection conn = null;
+        // Passo 1: Registrar driver JDBC.
+        Class.forName("org.apache.derby.jdbc.ClientDataSource");
+
+        // Passo 2: Abrir a conexão
+        conn = DriverManager.getConnection(
+                "jdbc:derby://localhost:1527/JavaHell;SecurityMechanism=3",
+                "", // usuario
+                ""); // senha
+        return conn;
+    }
+    
+}
